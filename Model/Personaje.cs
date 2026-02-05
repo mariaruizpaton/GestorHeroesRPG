ae2940f32ed795f60a2110db1042aa929ed43d34
Liviu <ldrotariu01@gmail.com>
2026-02-05 17:06:25 +0100

    [MOD] Model_Personaje

---
+++ Model/Personaje.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace GestorHeroesRPG.Model;
+
+[Table("character")]
+public abstract class Personaje
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Column("name")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Range(1, 100)]
+    [Column("level")]
+    public int Nivel { get; set; }
+
+    [Required]
+    [Column("creation_date")]
+    public DateOnly FechaCreation { get; set; }
+
+    [Column("guild")]
+    public string Gremio { get; set; } = string.Empty;
+}
