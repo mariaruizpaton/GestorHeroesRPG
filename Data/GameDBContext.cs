77a454b08d0c27b96283af489bab5ffdcf65dbe6
María <119685476+mariaruizpaton@users.noreply.github.com>
2026-02-05 16:59:42 +0100

    GameDBContext creado

---
+++ Data/GameDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace GestorHeroesRPG.Data;
+
+public class GameDBContext : DbContext
+{
+    public GameDBContext(DbContextOptions<GameDBContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5441;Database=D&DSoft;SearchPath=game;Username=ADMIN_GAME;Password=Abcd1234");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        modelBuilder.HasDefaultSchema("game");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
